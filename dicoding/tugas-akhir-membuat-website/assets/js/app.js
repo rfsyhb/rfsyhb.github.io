9e0eb4c97e76c91adfd37035f1d21df0170100c5
rfsyhb <rafisyihab3@gmail.com>
2024-02-28 00:04:14 +0700

    penambahan animasi scroll pada app.js

--- dicoding/tugas-akhir-membuat-website/assets/js/app.js
+++ dicoding/tugas-akhir-membuat-website/assets/js/app.js
@@ -1,3 +1,20 @@
+// ! button untuk pergi ke github
 document.getElementById('portoButton').addEventListener('click', function () {
     window.open('https://rfsyhb.github.io/', '_blank');
+});
+
+// ! animasi scroll
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
 });
