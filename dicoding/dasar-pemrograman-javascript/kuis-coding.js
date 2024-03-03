3ff5641f41216cdd33cd0d60aa9e4c3ea8d2962b
rfsyhb <rafisyihab3@gmail.com>
2024-03-03 16:35:21 +0700

    latihan js 1 dan 2

---
+++ dicoding/dasar-pemrograman-javascript/kuis-coding.js
@@ -0,0 +1,26 @@
+// ! Console log pertama
+// @TODO: Cetak teks "Saya mulai belajar JavaScript!" ke console
+console.log('Saya mulai belajar JavaScript!')
+
+// ! Variabel dan Tipe Data
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai "John".
+ *  - lastName: bertipe data string, dengan nilai "Doe".
+ *  - age: bertipe data number, dengan nilai 25.
+ *  - isMarried: bertipe data boolean, dengan nilai true.
+ */
+
+// Tulis kode di bawah ini
+// Deklarasi variabel dan nilai
+let firstName = 'John';
+let lastName = 'Doe';
+let age = 25;
+let isMarried = true;
+
+// Pengujian nilai
+console.log(`firstName nilainya ${firstName} dan tipe datanya ${typeof(firstName)}`)
+console.log(`lastName nilainya ${lastName} dan tipe datanya ${typeof(lastName)}`)
+console.log(`age nilainya ${age} dan tipe datanya ${typeof(age)}`)
+console.log(`isMarried nilainya ${isMarried} dan tipe datanya ${typeof(isMarried)}`)
