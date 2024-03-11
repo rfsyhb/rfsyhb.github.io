8a8afb6a30e10017f88a73ff811c4459931086ef
rfsyhb <rafisyihab3@gmail.com>
2024-03-11 19:18:37 +0700

    input event: onChange, onCopy, dan onPaste

--- dicoding/membuat-front-end-web-pemula/lat-9/index.js
+++ dicoding/membuat-front-end-web-pemula/lat-9/index.js
@@ -32,4 +32,35 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log('inputNama: blur');
         document.getElementById('notifikasiSisaKarakter').style.visibility = 'hidden';
     });
+
+    document.getElementById('inputCaptcha').addEventListener('change', function () {
+        console.log('inputCaptcha: change');
+        const inputCaptcha = document.getElementById('inputCaptcha').value;
+        const submitButtonStatus = document.getElementById('submitButton');
+        if (inputCaptcha === 'PRNU') {
+            submitButtonStatus.removeAttribute('disabled');
+        } else {
+            submitButtonStatus.setAttribute('disabled', '');
+        }
+    });
+
+    // ! tambahan: validasi ketika submit dan memastikan captcha
+    document.getElementById('formDataDiri').addEventListener('submit', function (event) {
+        const inputCaptcha = document.getElementById('inputCaptcha').value;
+        if (inputCaptcha === 'PRNU') {
+            alert('Selamat! Captcha Anda lolos :D');
+        } else {
+            alert('Captcha Anda belum tepat :(');
+            document.getElementById('submitButton').setAttribute('disabled', '');
+        }
+        event.preventDefault();
+    });
+
+    document.getElementById('inputCopy').addEventListener('copy', function () {
+        alert('Anda telah men-copy sesuatu...');
+    });
+
+    document.getElementById('inputPaste').addEventListener('paste', function () {
+        alert('Anda telah mem-paste sebuah teks...');
+    });
 });
