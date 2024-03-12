8275be4fa9f664a2cbdf8757aaddedf39acb8769
rfsyhb <rafisyihab3@gmail.com>
2024-03-12 11:20:30 +0700

    menambah event ketika load window selesai

--- dicoding/membuat-front-end-web-pemula/tebakangka/index.js
+++ dicoding/membuat-front-end-web-pemula/tebakangka/index.js
@@ -48,4 +48,27 @@ const sessionUserIsPlayingKey = "SESSION_USER_IS_PLAYING";
 const localTotalVictoryKey = "LOCAL_TOTAL_VICTORIES_PLAYED";
 const localMaximumAttemptsKey = "LOCAL_MAXIMUM_ATTEMPTS";
 
-
+// > Tahap kedua
+window.addEventListener("load", function () {
+  if (typeof Storage !== "undefined") {
+    // (MATERI 1)
+    // inisialisasi semua item web storage yang kita akan gunakan jika belum ada
+    if (sessionStorage.getItem(sessionAnswerKey) === null) {
+      sessionStorage.setItem(sessionAnswerKey, "");
+    }
+    if (sessionStorage.getItem(sessionUserAttemptsKey) === null) {
+      sessionStorage.setItem(sessionUserAttemptsKey, 0);
+    }
+    if (sessionStorage.getItem(sessionUserIsPlayingKey) === null) {
+      sessionStorage.setItem(sessionUserIsPlayingKey, false);
+    }
+    if (localStorage.getItem(localTotalVictoryKey) === null) {
+      localStorage.setItem(localTotalVictoryKey, 0);
+    }
+    if (localStorage.getItem(localMaximumAttemptsKey) === null) {
+      localStorage.setItem(localMaximumAttemptsKey, 0);
+    }
+  } else {
+    alert("Browser yang Anda gunakan tidak mendukung Web Storage");
+  }
+});
